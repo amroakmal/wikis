6384daadaf2723919ec9cf9278d26b491077fb5e
AmrAkmal <amrakmal97@gmail.com>
2020-05-01 06:38:34 +0200

    Showing current articles that has been created.

--- app.js
+++ app.js
@@ -19,9 +19,17 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
-   
-});
+app.get("/articles", function(req, res) {
+   Article.find(function(err, foundArticles) {
+    if(err) {
+        console.log("Error occured!");
+        res.send(err);
+    }
+    else {  
+        res.send(foundArticles);
+    }
+   });
+});  
 
 app.listen(3000, function() {
 
